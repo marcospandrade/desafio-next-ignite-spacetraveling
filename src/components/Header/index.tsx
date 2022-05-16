dc14564daac1aa6bbd53967de90bb1dc605c1638
Marcos Andrade <marcospandrade2@gmail.com>
2022-05-16 12:57:09 -0300

    build: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,29 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import Link from 'next/link';
+import { ActiveLink } from '../ActiveLink';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/">
+          <a>
+            <Image src="/images/logo.svg" alt="logo" height={26} width={210} />
+          </a>
+        </Link>
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
+          <ActiveLink href="/contact" activeClassName={styles.active}>
+            <a>Contact</a>
+          </ActiveLink>
+        </nav>
+      </div>
+    </header>
+  );
 }
