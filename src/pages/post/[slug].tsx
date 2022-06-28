d2734f84c5e420bef34f6b663af8b057b3964a0a
Marcos Andrade <marcospandrade2@gmail.com>
2022-06-27 23:36:32 -0300

    chore: getting post details and starting show

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,22 @@
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useCallback, useEffect, useState } from 'react';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+
+interface ContentProps {
+  heading: string;
+  body: {
+    text: string;
+  }[];
+}
 
 interface Post {
   first_publication_date: string | null;
@@ -13,12 +26,7 @@ interface Post {
       url: string;
     };
     author: string;
-    content: {
-      heading: string;
-      body: {
-        text: string;
-      }[];
-    }[];
+    content: ContentProps[];
   };
 }
 
@@ -26,20 +34,111 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post(): JSX.Element {
-  return <h1>Post</h1>;
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+  const [timeReading, setTimeReading] = useState<string>('');
+
+  const calculateReadingTime = useCallback(() => {
+    const wordsPerMinute = 225;
+    const words = post.data.content.reduce((acc, content) => {
+      return acc + content.body.length;
+    }, 0);
+    const minutes = Math.ceil(words / wordsPerMinute);
+    setTimeReading(`${minutes} min`);
+  }, [post.data.content]);
+
+  useEffect(() => {
+    calculateReadingTime();
+  }, [calculateReadingTime, post]);
+
+  console.log(post);
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{`Space T - ${post.data.title}`}</title>
+      </Head>
+      <img
+        className={styles.banner}
+        src={post?.data.banner.url}
+        alt={post.data.title}
+        height={400}
+        width="100%"
+      />
+      <div className={commonStyles.container}>
+        <h2 className={styles.headline}>{post?.data.title || 'Sem titulo'}</h2>
+        <div className={commonStyles.postInfo}>
+          <FiCalendar />
+          <time>
+            {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+              locale: ptBR,
+            })}
+          </time>
+          <FiUser />
+          <span>{post.data.author}</span>
+          <FiClock />
+          <span>{timeReading}</span>
+        </div>
+      </div>
+    </>
+  );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+  const posts = await prismic.getByType('post');
 
-//   // TODO
-// };
+  const postsPaths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      },
+    };
+  });
 
-// export const getStaticProps = async ({ params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths: postsPaths,
+    fallback: true,
+  };
+};
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  const PostInfo: Post = {
+    first_publication_date: format(
+      new Date(response.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content.map((detailContent: ContentProps) => {
+        return {
+          heading: detailContent.heading,
+          body: detailContent.body,
+        };
+      }),
+    },
+  };
+
+  return {
+    props: {
+      post: PostInfo,
+    },
+    redirect: 60 * 30, // 30 minutes
+  };
+};
